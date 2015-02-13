acad543372d4a2b2f5990bdf6835b13591510572
Fabian Lehner <fl@makonis.net>
2015-02-13 16:27:12 +0100

    styling possibility and opt-out field added.

--- djangocms_page_image/cms_plugins.py
+++ djangocms_page_image/cms_plugins.py
@@ -1,3 +1,6 @@
+from django.db.models import Q
+from django.template.loader import select_template
+
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
@@ -8,19 +11,29 @@ from .models import ChildPagePreviewPlugin
 
 class CMSChildPagePreviewPlugin(CMSPluginBase):
     model = ChildPagePreviewPlugin
-    
     name = _("Child Page Preview")
-    render_template = "djangocms_page_image/plugins/child_page_preview.html"
+    
+    TEMPLATE_NAME = "djangocms_page_image/plugins/%s.html"
+    render_template = TEMPLATE_NAME % 'child_page_preview'
     
     #Search
     search_fields = []
     
     def render(self, context, instance, placeholder):
+        self.render_template = select_template((
+            self.TEMPLATE_NAME % instance.style,
+            self.render_template,
+        ))
         request = context['request']
         if request.user.has_perm('cms.can_change'):
-            context['subpages'] = instance.placeholder.page.children.all()
+            subpages = instance.placeholder.page.children.all()
         else:
-            context['subpages'] = instance.placeholder.page.children.published()
+            subpages = instance.placeholder.page.children.published()
+        context['subpages'] = subpages.filter(
+            Q(imageextension__show_preview=True) |
+            Q(imageextension__isnull=True)
+        )
+        context['instance'] = instance
         return context
 
 plugin_pool.register_plugin(CMSChildPagePreviewPlugin)
--- djangocms_page_image/models.py
+++ djangocms_page_image/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 from filer.fields.image import FilerImageField
 
@@ -10,17 +11,36 @@ from cms.extensions import PageExtension
 from cms.extensions.extension_pool import extension_pool
 
 class ImageExtension(PageExtension):
-    image = FilerImageField(verbose_name=_('image'), blank=True)
+    image = FilerImageField(verbose_name=_('image'), blank=True, null=True)
     teaser = models.TextField(verbose_name=_('teaser'), blank=True)
+    show_preview = models.BooleanField(
+        verbose_name=_('show preview?'),
+        default=True
+    )
     
     class Meta:
         verbose_name=_('page image and teaser')
-    
-    def __unicode__(self):
-        return self.image.label
+
 
 extension_pool.register(ImageExtension)
 
 
 class ChildPagePreviewPlugin(CMSPlugin):
-    pass
+    STYLE_CHOICES = getattr(
+        settings,
+        'DJANGOCMS_PAGE_IMAGE_CPP_STYLE_CHOICES',
+        []
+    )
+    DEFAULT_STYLE = getattr(
+        settings,
+        'DJANGOCMS_PAGE_IMAGE_CPP_DEFAULT_STYLE',
+        ''
+    )
+    style = models.CharField(
+        _('style'),
+        choices=STYLE_CHOICES,
+        default=DEFAULT_STYLE,
+        max_length=50,
+        blank=True
+    )
+    
